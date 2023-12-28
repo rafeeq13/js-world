e9ef26f8305281a941b7e89f3332b7da3d508640
Rafeeq Mazari <131982124+rafeeq13@users.noreply.github.com>
2023-12-28 18:22:41 +0000

    datatypes confusions done

---
+++ 01_basics/03_datatypes_confusion.js
@@ -0,0 +1,8 @@
+let number='33abfjh'
+let numbernumber=Number(number)
+console.log(number)
+console.log(typeof(numbernumber))
+console.log(numbernumber)
+let boolnumber=Boolean(number)
+console.log(boolnumber)
+console.log(typeof boolnumber)
