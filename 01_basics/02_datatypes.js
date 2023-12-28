0b7d327f2241c31ad0f93a47ec5c25fc4f6b4802
Rafeeq Mazari <131982124+rafeeq13@users.noreply.github.com>
2023-12-28 17:57:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//string
+//number
+// null
+// undefined
+// Object
+// Boolean
+// BigInt
+// Symbol
+
+// examples
+let name='ali'
+let number=1
+let empId=null
+let designation;
+let x=true;
+console.table([name,number,empId,designation,x])
+// console.table(typeof[name,number,empId,designation,x])
